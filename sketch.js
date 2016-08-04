da1c7fe72b1d183f69f5dcc59401ff78558b7f38
Annaliese Gayle <ajgayle34@gmail.com>
2016-08-04 09:51:02 -0400

    create project

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function square(x, y, side) {
+  c = color(175, 100, 220); //DEFINE COLOR PURPLE
+  fill(c); //USE C AS FILL COLOR
+  rect(x, y, side, side);
+  }
+function setup() {
+  createCanvas(640, 480);
+    blueValue = blue(c);
+    println(blueValue);
+    fill(0, 0, blueValue);
+    rect(10, 10, 10, 15);
+  rect(40, 35, 10, 15);
+  noStroke();
+  colorMode(HSB, 255);
+  c = color(0, 126, 255);
+  fill(c);
+  rect(15, 20, 35, 60);
+  value = saturation(c);  // Sets 'value' to 126
+  fill(value);
+  rect(50, 20, 35, 60);
+  rect(60, 60, 45, 60);
+  
+  square(120,120,120,120);
+  
+}
+
+
+function draw() {
+  
+}
